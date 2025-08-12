9663cad30823460ebaa92292c1699f3d0412d26f
zoher <zoherkhan26@gmail.com>
2025-08-13 02:34:56 +0530

    Correction

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { SpeedInsights } from '@vercel/speed-insights/react'
-import {Analytics} from '@vercel/analytics'
+import {Analytics} from '@vercel/analytics/react'
 
 
 
